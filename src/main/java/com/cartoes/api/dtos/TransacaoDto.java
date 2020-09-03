e5f7d4e66a1510c8a05b7ac6b5ff9d1126ae1e67
Gabriel Brazão <gabrieelbrazao@gmail.com>
2020-09-03 18:20:39 -0300

    correction dtos

--- src/main/java/com/cartoes/api/dtos/TransacaoDto.java
+++ src/main/java/com/cartoes/api/dtos/TransacaoDto.java
@@ -73,6 +73,6 @@ public class TransacaoDto {
             + "valor=" + valor + ","
             + "qdtParcelas=" + qdtParcelas + ","
 			+ "juros=" + juros + ","
-			+ "cartao=" + cartao + "]";
+			+ "cartaoId=" + cartaoId + "]";
    	}
 }
